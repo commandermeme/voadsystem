18da13e27f37f41c90a182d41dcfc4a7dc7f02c1
commandermeme <carlobahog1@gmail.com>
2020-01-15 22:57:08 +0800

    Updating the api france

--- app/Http/Controllers/Api/TrackingsController.php
+++ app/Http/Controllers/Api/TrackingsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
+use App\Rule;
 
 class TrackingsController extends Controller
 {
@@ -16,9 +17,35 @@ class TrackingsController extends Controller
         // $responce = $client->request('GET', 'https://dev.virtualearth.net/REST/v1/Routes/SnapToRoad?points='. $lat .', '. $long .'&IncludeSpeedLimit=true&speedUnit=KPH&travelMode=driving&key=Arr8rRtGsDs4Wd5RmcwJlEBI9VeE7Zc00CfGaKhktcC87JwSfikFInKiVWRVVWKc');
 
         $partial_result = json_decode($responce->getBody(), true);
-        // $result = $partial_result['resourceSets'][0]['resources'][0]['snappedPoints'][0];
+        $result = $partial_result['resourceSets'][0]['resources'][0]['address']['addressLine'];
+       
+        $data = array(
+            'street' => $result,
+            'speed' => (int)$speed,
+            'system_id' => $system_id
+        );
 
-        return $partial_result;
+        $rule = Rule::where('street', $result)->get();
+
+        if (!$rule->isEmpty()) {
+            $data2 = array(
+                'street' => $rule[0]['street'],
+                'speed_limit' => $rule[0]['speed_limit'],
+                
+            );
+            
+            return $data2;
+        } else {
+            return 'wala sa database';
+        }
+        
+        
+        
+
+        // return $rule[0]['street'];
+
+        return $data2;
+        // return $partial_result;
     }
 
     
